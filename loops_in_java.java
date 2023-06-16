39d52d20ce17f6363284efa7a1e00246d56552fc
Soma Sekhar Udatha <115133440+somasekhar-udatha@users.noreply.github.com>
2023-06-16 22:46:44 +0530

    Create loops_in_java.java

---
+++ loops_in_java.java
@@ -0,0 +1,32 @@
+public class Loops_in_Java {
+    public static void main(String[] args) {
+        // While loop
+        /*int i = 100;
+        while(i <= 200) {
+            System.out.println(i);
+            i++;
+        }*/
+        int j = 1;
+        /*// do while loop
+        do {
+            System.out.println(j++);
+        }while(j<=5);
+
+         */
+        /*int k = 1;
+        do {
+            System.out.println(k++);
+        }while(k<=100);
+         */
+        /*for(int i = 0;i <= 50;i++) {
+            if (i%2 != 0) {
+                System.out.println(i);
+            }
+
+        }
+         */
+        for(int i = 10;i >= 1;i--) {
+            System.out.println(i);
+        }
+    }
+}
